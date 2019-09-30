108919075745dd09ef38432afd778ffa2db90cba
LuisOvalle <lpovalle26@gmail.com>
2019-09-30 10:26:49 -0600

    Validaciones
    Falta mejorar el diseño

--- src/app/components/instrumento-form/instrumento-form.component.ts
+++ src/app/components/instrumento-form/instrumento-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { InstrumentoService} from '../../services/instrumento.service'
 import { Instrumento } from '../../models/Instrumento';
 import { ActivatedRoute, Router } from '@angular/router'
+import { NgForm } from '@angular/forms';
 
 
 @Component({
@@ -14,6 +15,9 @@ export class InstrumentoFormComponent implements OnInit {
   public nuevoInstrumento: Instrumento;
   public editar: boolean = false;
   parametros;
+  mensaje:string
+  public mosrarMensaje = false
+  public nombreViejo ="";
 
   constructor(
     public instrumentoServicio: InstrumentoService,
@@ -25,6 +29,7 @@ export class InstrumentoFormComponent implements OnInit {
     this.parametros = this.activatedRoute.snapshot.params;
     if(this.parametros.id){
       this.nuevoInstrumento = this.instrumentoServicio.getInstrumento(this.parametros.id);
+      this.nombreViejo = this.nuevoInstrumento.nombre;
       this.editar = true;
       console.log(this.editar);
     } else {
@@ -32,13 +37,30 @@ export class InstrumentoFormComponent implements OnInit {
     }
   }
 
-  addInstrumento(){
-    this.instrumentoServicio.addInstrumento(this.nuevoInstrumento);
-    this.router.navigate(['/instrumentos']);
+  addInstrumento(form: NgForm){
+    if(form.valid==true){
+      this.instrumentoServicio.addInstrumento(this.nuevoInstrumento);
+      this.router.navigate(['/instrumentos']);
+    } else {
+      this.mensaje="Por favor corregir campos."
+      this.mosrarMensaje=true;
+      setTimeout(()=>{
+        this.mosrarMensaje=false
+      },5000  );
+    }
+
+    
   }
 
-  updateInstrumento(){
-    this.instrumentoServicio.updateInstrumento(this.nuevoInstrumento, this.parametros.id);
-    this.router.navigate(['/instrumentos']);
+  updateInstrumento(form: NgForm){
+    if(form.valid==true){
+      if(confirm('¿Desea sobre escribir "' + this.nombreViejo + '"?')){
+        this.instrumentoServicio.updateInstrumento(this.nuevoInstrumento, this.parametros.id);
+        this.router.navigate(['/instrumentos']);
+      }    
+    } else {
+
+    }
+    
   }
 }
